9571e7a6f80bcb495e4526909525d07ee81f99f4
Jayesh Bansal <jayeshbansal2001@gmail.com>
2024-03-22 19:01:24 +0530

    conversion of data type

---
+++ datatype_conversion.js
@@ -0,0 +1,30 @@
+let score = 12
+
+// Values
+
+// null > 0
+// undefined > NaN
+// True > 1
+// False > 0
+//"" > 0 //empty string, boolean output
+// Anyword > NaN
+
+//To check data type
+
+//way 1 
+console.log(typeof score);
+
+
+//way 2 as a method
+console.log(typeof(score));
+
+
+// Datatype Conversion
+
+// Number(Variable_name)
+// String(Varibale_name)
+// Boolean(Varibale_name)
+
+let valueInNumber = String(score)
+
+console.log(typeof valueInNumber);
